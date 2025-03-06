3363dc87a1872e581e6beda58c6b74b3e4b541cb
Olivia Proust <olivia.proust@etudiant.univ-rennes1.fr>
2025-03-06 14:35:55 +0100

    Modification de grammaire pour check des fonctions

--- B0TCH extension/src/language/b-0-tch-validator.ts
+++ B0TCH extension/src/language/b-0-tch-validator.ts
@@ -1,5 +1,5 @@
-import type { ValidationChecks } from 'langium';
-import type { B0TchAstType } from './generated/ast.js';
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import type { B0TchAstType, DefFonction, Robot } from './generated/ast.js';
 import type { B0TchServices } from './b-0-tch-module.js';
 
 /**
@@ -9,7 +9,7 @@ export function registerValidationChecks(services: B0TchServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.B0TchValidator;
     const checks: ValidationChecks<B0TchAstType> = {
-
+        DefFonction: validator.checkDefFunction
     };
     registry.register(checks, validator);
 }
@@ -19,6 +19,20 @@ export function registerValidationChecks(services: B0TchServices) {
  */
 export class B0TchValidator {
 
+    checkDefFunction(defFonction: DefFonction, accept: ValidationAcceptor) {
+        // Récupérer le parent qui est censé être le `Robot`
+        const robot = defFonction.$container as Robot;
 
+        if (!robot) {
+            console.warn(`Impossible de trouver le conteneur de la fonction ${defFonction.name}`);
+            return;
+        }
 
+        // Vérifier les doublons en se basant sur `robot`
+        const duplicateCount = robot.bloc.filter(f => f.name === defFonction.name).length;
+
+        if (duplicateCount > 1) {
+            accept('warning', `La fonction "${defFonction.name}" est déjà définie.`, { node: defFonction, property: 'name' });
+        }
+    }
 }
